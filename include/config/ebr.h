4dd19b7e8c62ff91acf51e89f4486d7be9ea8db6
Damon Murdoch <damonmurdoch1@gmail.com>
2026-01-15 17:50:45 +1000

    Game balance changes
    Doubled cost for cherish ball / gold bottle cap, reduced starting money, enabled pokemon box link item

--- include/config/ebr.h
+++ include/config/ebr.h
@@ -2,12 +2,12 @@
 #define GUARD_CONFIG_EBR_H
 
 #define EBR_QUICK_START_ENABLED     TRUE    // If true, use quick start routine at battle frontier
-#define EBR_QUICK_START_MONEY       500000  // Player Starting Money
+#define EBR_QUICK_START_MONEY       47000   // Player Starting Money
 
 #define EBR_QUICK_START_CHARMS      TRUE    // Shiny Charm, etc. given via quick start
 #define EBR_QUICK_START_EVENT_ITEMS TRUE    // Event-exclusive items given via quick start
 
-#define EBR_POKEMON_BOX_LINK_ENABLED    FALSE    // Pokemon Box Link Item Enabled
+#define EBR_POKEMON_BOX_LINK_ENABLED    TRUE    // Pokemon Box Link Item Enabled
 
 #define EBR_HARD_MODE_BONUS 5   // Bonus 'challengeNum' added to BP earned playing hard mode
 
