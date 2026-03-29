cf3f5b401d4de897f4aae19ea33805d0b2273ef3
Damon Murdoch <damonmurdoch1@gmail.com>
2026-03-29 21:53:07 +1000

    Removed day/night based evolutions, and replaced day/night split evolutions with sun/moon/dusk stones as applicable
    Removed day/night based evolutions, and replaced day/night split evolutions with sun/moon/dusk stones as applicable

--- src/data/pokemon/species_info/gen_6_families.h
+++ src/data/pokemon/species_info/gen_6_families.h
@@ -4567,7 +4567,7 @@ const struct SpeciesInfo gSpeciesInfoGen6[] =
         .levelUpLearnset = sAmauraLevelUpLearnset,
         .teachableLearnset = sAmauraTeachableLearnset,
         .eggMoveLearnset = sAmauraEggMoveLearnset,
-        .evolutions = EVOLUTION({EVO_LEVEL, 39, SPECIES_AURORUS, CONDITIONS({IF_TIME, TIME_NIGHT})}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 39, SPECIES_AURORUS}),
     },
 
     [SPECIES_AURORUS] =
