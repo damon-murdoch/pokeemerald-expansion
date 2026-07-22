7c8358fb9ea8cf7953006415f1f043d87c687517
Damon Murdoch <damonmurdoch1@gmail.com>
2026-07-22 23:54:59 +1000

    Implemented battle factory type checker, battle style predictor
    Implemented battle factory type checker, battle style predictor for battle frontier generator

--- include/config/battle_frontier_generator.h
+++ include/config/battle_frontier_generator.h
@@ -118,6 +118,16 @@
 
 #define BFG_FACTORY_EXPERT_CHALLENGE_NUM 7
 
+// Battle Factory Battle Styles
+
+#define BFG_FACTORY_BATTLE_STYLE_PREPARATION_BST 1440 // 500x3
+#define BFG_FACTORY_BATTLE_STYLE_SLOW_AND_STEADY_SPE 210 // 60x3
+#define BFG_FACTORY_BATTLE_STYLE_WEAKENING_ATK_SPATK 420 // 140X3
+
+// If hp/def/spdef and atk/spa/spe are within this threshold, 
+// will use one of the more generic battle style predictions
+#define BFG_FACTORY_BATTLE_STYLE_COMPARE_THRESHOLD 60
+
 // *** BASE STATS ***
 
 #define BFG_IV_FIXED 31 // Fixed IVs when 'FLAG_BATTLE_FRONTIER_FIXED_IV' is set
@@ -330,7 +340,7 @@
                                     // i.e. attack and speed ivs will be set
                                     // to 0 if a reducing nature is used
 
-                                    #define BFG_OPTIMISE_IVS_NO_ATTACKS TRUE    // If set to true, the attack stat will
+#define BFG_OPTIMISE_IVS_NO_ATTACKS TRUE    // If set to true, the attack stat will
                                             // be set to 0 if the mon does not have
                                             // any physical attacks (and has not 
                                             // previously been set to 0.)
@@ -436,7 +446,7 @@
 #define BFG_ITEM_BLUNDER_POLICY_SELECTION_CHANCE    16  // numInaccurate
 #define BFG_ITEM_PUNCHING_GLOVE_SELECTION_CHANCE    16  // numPunch
 #define BFG_ITEM_ROOM_SERVICE_SELECTION_CHANCE      8   // hasTrickRoom
-#define  BFG_ITEM_CHESTO_BERRY_SELECTION_CHANCE      1   // hasRest
+#define BFG_ITEM_CHESTO_BERRY_SELECTION_CHANCE      1   // hasRest
 #define BFG_ITEM_THROAT_SPRAY_SELECTION_CHANCE      4   // numSound
 #define BFG_ITEM_LOADED_DICE_SELECTION_CHANCE       4   // numMultiHit
 #define BFG_ITEM_LIGHT_CLAY_SELECTION_CHANCE        4   // numScreens
